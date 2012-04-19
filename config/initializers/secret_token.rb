e4793522169ffffbd47dc873ff37a15bb14d7ecb
Tejas Garde <tejasgarde@yahoo.co.in>
2012-04-19 10:03:34 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveApp::Application.config.secret_token = 'c171e9f1324fa86470f89d6f8d3a8756ef75d5fffd91ff026ef230f2f05627157f651e8e3dec6a0109cb227124d5c00b3b1fa86758dd1dde9d9e50da708a29cf'
