13f726e1e17f2dcf28c45f0cb44a2673d81d8b7f
Tejas Garde <tejasgarde@yahoo.co.in>
2012-04-20 15:25:23 +0530

    Added CSS & jQuery Plugins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,22 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+$(document).ready(function() {
+		var carousel = $("#carousel").featureCarousel({
+		  // include options like this:
+		  // (use quotes only for string values, and no trailing comma after last option)
+		  // option: value,
+		  // option: value
+		});
+
+		$("#but_prev").click(function () {
+		  carousel.prev();
+		});
+		$("#but_pause").click(function () {
+		  carousel.pause();
+		});
+		$("#but_start").click(function () {
+		  carousel.start();
+		});
+		$("#but_next").click(function () {
+		  carousel.next();
+		});
+ });
+   
